adaad5a7bf27dbf4e2c3b205a74554504b7cd4a6
elelanv <vickneshelelan@gmail.com>
2026-08-14 03:49:04 +0530

    feat(client): add adaptive login layouts

--- app/shared/src/commonMain/kotlin/dev/elelan/quotequiz/ui/core/AdaptiveWindowLayout.kt
+++ app/shared/src/commonMain/kotlin/dev/elelan/quotequiz/ui/core/AdaptiveWindowLayout.kt
@@ -3,18 +3,26 @@ package dev.elelan.quotequiz.ui.core
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
+import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
 import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_EXPANDED_LOWER_BOUND
+
+enum class AdaptiveLayoutSize {
+    Compact,
+    Medium,
+    Expanded,
+}
 
 @Composable
 fun AdaptiveWindowLayout(
-    compatMediumContent: @Composable () -> Unit,
-    expandedContent: @Composable () -> Unit
+    compactContent: @Composable () -> Unit,
+    mediumContent: @Composable () -> Unit,
+    expandedContent: @Composable () -> Unit,
 ) {
     BoxWithConstraints {
-        if (maxWidth <= WIDTH_DP_EXPANDED_LOWER_BOUND.dp) {
-            compatMediumContent()
-        } else {
-            expandedContent()
+        when {
+            maxWidth < WIDTH_DP_MEDIUM_LOWER_BOUND.dp -> compactContent()
+            maxWidth < WIDTH_DP_EXPANDED_LOWER_BOUND.dp -> mediumContent()
+            else -> expandedContent()
         }
     }
 }
