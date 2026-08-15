4ea0dddef2d0af0c9f5fa3097095e2c19a372101
elelanv <vickneshelelan@gmail.com>
2026-08-15 18:35:16 +0530

    fix(server): removed duplicated cors config

--- server/src/main/kotlin/dev/elelan/quote_quiz_server/SecurityConfig.kt
+++ server/src/main/kotlin/dev/elelan/quote_quiz_server/SecurityConfig.kt
@@ -73,7 +73,4 @@ class SecurityConfig(
             registerCorsConfiguration("/**", configuration)
         }
     }
-
-    @Bean
-    fun corsFilter(): CorsFilter = CorsFilter(corsConfigurationSource())
 }
