070283b53ba26d288c9da1bb53d1ef1355966b86
elelanv <vickneshelelan@gmail.com>
2026-08-15 16:49:26 +0530

    chore: added github to cors

--- server/src/main/kotlin/dev/elelan/quote_quiz_server/CorsConfig.kt
+++ server/src/main/kotlin/dev/elelan/quote_quiz_server/CorsConfig.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 class CorsConfig(
-    @Value("\${quotequiz.cors.allowed-origin-patterns:http://localhost:*,http://127.0.0.1:*,https://localhost:*,https://127.0.0.1:*,https://quote-quiz.onrender.com}")
+    @Value("\${quotequiz.cors.allowed-origin-patterns:http://localhost:*,http://127.0.0.1:*,https://localhost:*,https://127.0.0.1:*,https://quote-quiz.onrender.com,https://*.github.io}")
     private val allowedOriginPatterns: String,
 ) {
     val allowedOriginPatternsList: List<String>
